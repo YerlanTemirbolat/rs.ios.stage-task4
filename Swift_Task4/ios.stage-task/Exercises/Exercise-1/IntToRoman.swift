c3c2fd412ab1a89a670faf2c5b02063395493e4e
YerlanTemirbolat <erlantemirbolat87@gmail.com>
2021-06-06 18:09:28 +0600

    feat: solution of the tasks

--- Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-1/IntToRoman.swift
@@ -3,6 +3,25 @@ import Foundation
 public extension Int {
     
     var roman: String? {
-        nil
+
+        if self >= 1 && self <= 3999 {
+
+            var number = self
+            var romanValue = ""
+
+            let list: [(Int, String)] = [(1000, "M"), (900, "CM"), (500, "D"), (400, "CD"),
+                                              (100, "C"), (90, "XC"), (50, "L"), (40, "XL"),
+                                              (10, "X"), (9, "IX"), (5, "V"), (4, "IV"), (1, "I")]
+
+            for i in list {
+                while (number >= i.0) {
+                    number -= i.0
+                    romanValue += i.1
+                }
+            }
+            return romanValue
+        } else {
+            return nil
+        }
     }
 }
--- Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
+++ Swift_Task4/ios.stage-task/Exercises/Exercise-3/CallStation.swift
@@ -1,37 +1,119 @@
 import Foundation
 
-final class CallStation { }
+final class CallStation {
+    var myUsers: [User] = []
+    var myCalls: [Call] = []
+}
 
 extension CallStation: Station {
+
     func users() -> [User] {
-        []
+        return myUsers
     }
     
     func add(user: User) {
-
+        if !self.myUsers.contains(user) {
+            self.myUsers.append(user)
+        }
     }
     
     func remove(user: User) {
-
+        let index = myUsers.firstIndex(where: {$0.id == user.id})
+        if index != nil {
+            myUsers.remove(at: index!)
+        }
     }
-    
+
     func execute(action: CallAction) -> CallID? {
-        nil
+
+        switch action {
+        case .start(from: let incomingUser, to: let outgoingUser):
+
+            if !myUsers.contains(incomingUser) && !myUsers.contains(outgoingUser) {
+                return nil
+            } else if !myUsers.contains(outgoingUser) {
+                let errorUUID = UUID.init()
+                let myCall = (Call(id: errorUUID, incomingUser: incomingUser, outgoingUser: outgoingUser, status: .ended(reason: .error)))
+                myCalls.append(myCall)
+                return errorUUID
+            }
+
+            if self.currentCall(user: outgoingUser) != nil {
+                let busyUUID = UUID.init()
+                let myCall = (Call(id: busyUUID, incomingUser: incomingUser, outgoingUser: outgoingUser, status: .ended(reason: .userBusy)))
+                myCalls.append(myCall)
+                return busyUUID
+            }
+
+            let myId = UUID.init()
+            let myCall = (Call(id: myId, incomingUser: incomingUser, outgoingUser: outgoingUser, status: .calling))
+            self.myCalls.append(myCall)
+
+            return myId
+
+        case .answer(from: let incomingUser):
+
+            // && $0.status == .calling
+            let myId = myCalls.first(where: {$0.outgoingUser.id == incomingUser.id})?.id
+
+            let callIndex = self.myCalls.firstIndex(where: {$0.id == myId && $0.status == .calling})
+
+            guard let index = callIndex else {
+                return nil
+            }
+
+            guard myUsers.contains(myCalls[index].outgoingUser) else {
+                
+                myCalls[index].status = .ended(reason: .error)
+                return nil
+            }
+
+            myCalls[index].status = .talk
+
+
+            return myId
+
+        case .end(from: let outgoingUser):
+
+            let myId = calls(user: outgoingUser).first?.id
+            
+            let cancelIndex = self.myCalls.firstIndex(where: {$0.id == myId && $0.status == .calling})
+            if cancelIndex != nil {
+                myCalls[cancelIndex!].status = .ended(reason: .cancel)
+            }
+
+            let endIndex = self.myCalls.firstIndex(where: {$0.id == myId && $0.status == .talk})
+            if endIndex != nil {
+                myCalls[endIndex!].status = .ended(reason: .end)
+            }
+
+            return myId
+        }
     }
     
     func calls() -> [Call] {
-        []
+        myCalls
     }
     
     func calls(user: User) -> [Call] {
-        []
+        let calls = myCalls.filter { call in
+            call.incomingUser.id == user.id || call.outgoingUser.id == user.id
+        }
+        return calls
     }
     
     func call(id: CallID) -> Call? {
-        nil
+        let myCall = myCalls.filter { itemCall in
+            itemCall.id == id
+        }
+        return myCall.first ?? nil
     }
     
     func currentCall(user: User) -> Call? {
-        nil
+        let call = myCalls.filter { call in
+            (call.incomingUser.id == user.id || call.outgoingUser.id == user.id) &&
+                (call.status == .calling || call.status == .talk)
+        }
+        return call.first
     }
 }
